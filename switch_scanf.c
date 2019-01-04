92e1f0d965983a2588ba725564c48feb041b3673
seokhoonj <seokhoonj@gmail.com>
2019-01-05 01:52:26 +0900

    add new files

---
+++ switch_scanf.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+
+	scanf("%d", &num1);
+
+	switch (num1)
+	{
+		case 1:
+			printf("1입니다.\n");
+			break;
+		case 2:
+			printf("2입니다.\n");
+			break;
+		defalut:
+			printf("default\n");
+			break;
+	}
+
+	return 0;
+}
