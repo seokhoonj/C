aa21163e71185fc2c4b708734f53f3e63a6a20fc
seokhoonj <seokhoonj@gmail.com>
2019-01-05 16:26:08 +0900

    add new files

---
+++ memory_set.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	long long *numPtr = malloc(sizeof(long long));
+
+	memset(numPtr, 0x27, 8);
+
+	printf("0x%llx\n", *numPtr);
+
+	free(numPtr);
+
+	return 0;
+}
