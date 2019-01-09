6d7cf73d5c676cf4544dc7daaba21bf9e30e6471
seokhoonj <seokhoonj@gmail.com>
2019-01-10 00:19:19 +0900

    add new files

---
+++ function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void hello()
+{
+	printf("Hello, world!\n");
+}
+
+int main()
+{
+	hello();
+
+	return 0;
+}
---
+++ function_prototype.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void hello();
+
+int main()
+{
+	hello();
+
+	return 0;
+}
+
+void hello()
+{
+	printf("Hello, world!\n");
+}
